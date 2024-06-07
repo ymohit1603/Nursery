9c4bf208f0954cf62a70b591f476b0d363e0ff8f
ymohit1603 <mohityadav0330@gmail.com>
2024-06-07 15:01:44 +0530

    added prisma

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.get("/signup", (req: Request, res: Response) => {
     res.send("Signup Page for User");
+    
 });
 
 router.get("/signin", (req: Request, res: Response) => {
