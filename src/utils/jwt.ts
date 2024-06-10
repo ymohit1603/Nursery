eddb19d4d78a8fdc174520786cdd0947194b3703
ymohit1603 <mohityadav0330@gmail.com>
2024-06-10 13:28:11 +0530

    Added util

---
+++ src/utils/jwt.ts
@@ -0,0 +1,27 @@
+import { error } from "console";
+import jwt, { JsonWebTokenError } from "jsonwebtoken";
+const jwtSecret = process.env.SECRET;
+export const signToken = (payload: object): string => {
+    try {
+        if (!jwtSecret) {
+            throw error("jwt secret required");
+        }
+        return jwt.sign(payload, jwtSecret, { expiresIn: '1h' });
+    }
+    catch (error) {
+        console.error("Error signing token:", error.message);
+        throw error;
+    }
+};
+export const verifyToken = (token: string) => {
+    try {
+        if (!jwtSecret) {
+            throw error("jwt secret required");
+        }
+        return jwt.verify(token, jwtSecret);
+    }
+    catch (error) {
+        console.log("wrong token", error);
+        throw error;
+    }
+  };
