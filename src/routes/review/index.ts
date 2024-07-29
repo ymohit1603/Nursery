153f43f78f6dd5effa1b009900035803db992102
ymohit1603 <mohityadav0330@gmail.com>
2024-07-29 14:46:43 +0530

    add: added review route

---
+++ src/routes/review/index.ts
@@ -0,0 +1,56 @@
+import express from "express";
+import prisma from "../../utils/prisma";
+
+
+
+const router = express.Router();
+
+router.get('/:productId', async (req, res) => {
+    const { productId } = req.params;
+    
+    if (!productId) {
+        return res.status(400).json({ message: "productId required" });
+    }
+
+    try {
+        const plantWithReviews = await prisma.buyPlant.findUnique({
+            where: { id: parseInt(productId) },
+            include: { review: true }, 
+        });
+
+        if (!plantWithReviews) {
+            return res.status(404).json({ message: "Plant not found" });
+        }
+
+        return res.status(200).json(plantWithReviews);
+    } catch (e) {
+        console.error(e);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+})
+
+router.post('/',async (req, res) => {
+    const data = req.body;
+    if (!data) {
+        return res.status(400).json({ message: "Info required" });
+    }
+    const { productId, name, email, rating, review } = data;
+
+    try {
+        const newReview = await prisma.review.create({
+            data: {
+              name,
+              email,
+              rating,
+              review,
+              plant: {
+                connect: { id: productId },
+              },
+            },
+          });
+    } catch (error){
+        return res.status(500).json({ message: "Internal server error" });
+    }
+})
+
+export default router;
